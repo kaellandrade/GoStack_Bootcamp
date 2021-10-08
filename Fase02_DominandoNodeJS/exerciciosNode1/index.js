84ae00c18106faba0aa648f4b79cf118ed960a6d
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-10-08 20:09:48 -0300

    put e delete

--- Fase02_DominandoNodeJS/exerciciosNode1/index.js
+++ Fase02_DominandoNodeJS/exerciciosNode1/index.js
@@ -2,7 +2,7 @@ import Express from 'express';
 const server = Express();
 server.use(Express.json());
 
-const projetos = [
+let projetos = [
 ];
 
 /**
@@ -18,11 +18,35 @@ server.get('/projects', (_, res) => {
 server.post('/projects', (req, res) => {
 
     const { id, title } = req.body;
-
-    projetos.push({id, title, tasks:[]})
-    
+    projetos.push({ id, title, tasks: [] })
     res.json(projetos)
 });
 
+/**
+ * Atualiza o título do projeto cujo ID é passado
+ * nos parâmetros.
+ */
+server.put('/projects/:id', (req, res) => {
+    const idRecebido = req.params.id;
+    const titleRecebido = req.body.title;
 
+    projetos = projetos.map((projeto) => {
+        if (idRecebido == projeto.id) {
+            return { ...projeto, title: titleRecebido };
+        }
+        return projeto
+    });
+    res.json(projetos);
+});
+
+/**
+ * Recebe um id e delete o usário com esse ID
+ */
+
+server.delete('/projects/:id', (req, res) => {
+    const idRecebido = req.params.id;
+    projetos = projetos.filter((projeto) => (idRecebido != projeto.id))
+
+    res.send();
+});
 server.listen(4000);
