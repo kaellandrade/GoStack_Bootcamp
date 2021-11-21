fff7f9bb55299d6ab57409bdea6ed8f73d18f076
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-11-20 22:57:35 -0300

    update de usuários

--- Fase02_DominandoNodeJS/desafio2/_gympoint/src/app/controllers/UserController.js
+++ Fase02_DominandoNodeJS/desafio2/_gympoint/src/app/controllers/UserController.js
@@ -11,8 +11,34 @@ class UserController {
     return res.status(400).send({ error: 'Usuário já existe!' });
   }
 
+  /**
+   * Atualiza dados do usuário apenas se estiver logado.
+   * (Utilizando o meddleware de autenticação)
+   */
   async update(req, res) {
-    return res.json({ message: req.userId });
+    const { email, oldPassword } = req.body;
+    const userUpdate = await User.findByPk(req.userId);
+
+    if (email !== userUpdate.email) {
+      const user = await User.findOne({ where: { email } });
+      if (user) {
+        return res
+          .status(400)
+          .send({ error: 'Já existe um usuário com esse email' });
+      }
+    }
+    /**
+     * checkPassword só será verificada se o usuário deseja alterar a senha.
+     */
+    if (oldPassword && !(await userUpdate.checkPassword(oldPassword))) {
+      return res.status(401).send({ error: 'Senha antiga está inválida!' });
+    }
+
+    // Tudo deu certo até aqui
+
+    const { name, id, admin } = await userUpdate.update(req.body);
+
+    return res.json({ name, id, admin, email });
   }
 }
 
