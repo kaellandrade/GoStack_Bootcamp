8141d46f739fb83893cc701f53b48a729d684505
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-11-23 22:30:00 -0300

    métodos delete e put em StudentControler

--- Fase02_DominandoNodeJS/desafio2/_gympoint/src/app/controllers/StudentsController.js
+++ Fase02_DominandoNodeJS/desafio2/_gympoint/src/app/controllers/StudentsController.js
@@ -18,5 +18,65 @@ class StudentsController {
       });
     }
   }
+
+  async update(req, res) {
+    const { email, new_email } = req.body;
+    try {
+      const students = await Students.findOne({
+        where: { email },
+      });
+      if (!students) {
+        // Verificando se usuário existe
+        return res
+          .status(401)
+          .send({ error: 'Impossível atulizar, pois estudante não existe!' });
+      }
+      if (new_email && email !== new_email) {
+        // Deseja alterar o email
+        const exist_email = await Students.findOne({
+          where: { email: new_email },
+        });
+        if (!exist_email) {
+          // Novo email não  existe no banco. OK
+          const { name, data_nascimento, peso, altura } = await students.update(
+            {
+              ...req.body,
+              email: new_email,
+            },
+          );
+          return res.json({ name, new_email, data_nascimento, peso, altura });
+        }
+        return res
+          .status(400)
+          .send({ error: `Já existe usuário com o email ${new_email}` });
+      }
+      // Não deseja alterar o email
+      const { name, data_nascimento, peso, altura } = await students.update(
+        req.body,
+      );
+      return res.json({ name, email, data_nascimento, peso, altura });
+    } catch (error) {
+      return res.status(400).send({ error: error.message });
+    }
+  }
+
+  /**
+   * Removendo um novo usuário
+   */
+  async delete(req, res) {
+    const { email } = req.body;
+    try {
+      const student = await Students.findOne({ where: { email } });
+      if (student) {
+        await student.destroy();
+        return res.json({
+          messagem: `Usuário ${student.name} removido com sucesso!`,
+        });
+      }
+      return res.status(400).send({ messagem: `${email} não existe! ` });
+    } catch (error) {
+      return res.json(error);
+    }
+  }
 }
 export default new StudentsController();
