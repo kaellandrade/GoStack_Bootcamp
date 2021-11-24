69dbd9f6b5abf0bd5d15c7e2c9d1bab45dcb0075
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-11-23 23:31:39 -0300

    validando dados de cadastro e atualização de usuário

--- Fase02_DominandoNodeJS/desafio2/_gympoint/src/app/controllers/UserController.js
+++ Fase02_DominandoNodeJS/desafio2/_gympoint/src/app/controllers/UserController.js
@@ -1,7 +1,21 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(8),
+      admin: Yup.bool(),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res
+        .status(401)
+        .json({ error: 'Impossível cadastrar usário. Dados inválidos.' });
+    }
+
     const userExist = await User.findOne({ where: { email: req.body.email } });
     if (!userExist) {
       const { id, name, email, admin } = await User.create(req.body);
@@ -16,6 +30,26 @@ class UserController {
    * (Utilizando o meddleware de autenticação)
    */
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      admin: Yup.bool(),
+      oldPassword: Yup.string().min(8),
+      password: Yup.string()
+        .min(8)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field,
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([password, field]) : field,
+      ),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: 'Dados inválido. Verifique os dados e tente novamente',
+      });
+    }
+
     const { email, oldPassword } = req.body;
     const userUpdate = await User.findByPk(req.userId);
 
