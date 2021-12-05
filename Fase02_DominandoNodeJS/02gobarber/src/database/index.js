49215c126f755f1aad2f0a7afa6c2407fdcec539
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-12-04 21:07:18 -0300

    conectando com mongo

--- Fase02_DominandoNodeJS/02gobarber/src/database/index.js
+++ Fase02_DominandoNodeJS/02gobarber/src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
@@ -13,6 +14,7 @@ const models = [User, File, Appointment];
 class Database {
     constructor() {
         this.init();
+        this.mongo();
     }
     init() {
         // Minha conexão com o banco de dados.
@@ -23,6 +25,15 @@ class Database {
                 (model) =>
                     model.associate && model.associate(this.connection.models),
             );
+    }
+    mongo() {
+        this.mongoConnection = mongoose.connect(
+            'mongodb://localhost:27017/gobarber',
+            {
+                // useNewUrlParser: true,
+                // useFindAndModify: true,
+            },
+        );
     }
 }
 
