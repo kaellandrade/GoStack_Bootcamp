d9e77c44725808918dba93040390c129183db5ef
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-12-10 23:53:36 -0300

    configurando filas de jobs eAppointmentController

--- Fase02_DominandoNodeJS/02gobarber/src/lib/Queue.js
+++ Fase02_DominandoNodeJS/02gobarber/src/lib/Queue.js
@@ -29,8 +29,11 @@ class Queue {
         jobs.forEach((job) => {
             const { bee, handle } = this.queues[job.key];
 
-            bee.process(handle);
+            bee.on('failed', this.handleFailure).process(handle);
         });
+    }
+    handleFailure(job, err) {
+        console.log(`Queue ${job.queue.name}: FAILED`, err);
     }
 }
 export default new Queue();
