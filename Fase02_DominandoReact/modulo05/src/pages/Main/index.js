0cccdc4c69dced48f99d9f43229a27aa49f1d2a9
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-12-30 16:57:06 -0300

    recuperando repo do github

--- Fase02_DominandoReact/modulo05/src/pages/Main/index.js
+++ Fase02_DominandoReact/modulo05/src/pages/Main/index.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BsGithub, BsPlusLg } from 'react-icons/bs';
+import { FaSpinner } from 'react-icons/fa';
+import API from '../../services/api';
+import { Container, Form, SubmitButton } from './styles';
 
-function Main() {
-    return <h1>Main</h1>;
-}
+const Main = () => {
+    const [newRepo, setNewRepo] = useState('');
+    const [repositories, setRepositories] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        const response = await API.get(`repos/kaellandrade/${newRepo}`);
+        const data = {
+            name: response.data.full_name,
+            description: response.data.description,
+            created_at: response.data.created_at,
+        };
+
+        setRepositories([...repositories, data]);
+        setNewRepo('');
+        setLoading(false);
+    };
+
+    return (
+        <Container>
+            <h1>
+                <BsGithub /> Repositórios
+            </h1>
+            <Form onSubmit={handleSubmit}>
+                <input
+                    value={newRepo}
+                    type="text"
+                    placeholder="Adicionar Repositório"
+                    onChange={(e) => setNewRepo(e.target.value)}
+                />
+                <SubmitButton loading={loading ? 1 : undefined}>
+                    {loading ? (
+                        <FaSpinner color="#FFF" size={14} />
+                    ) : (
+                        <BsPlusLg className='spinner' color="#FFF" size={14} />
+                    )}
+                </SubmitButton>
+            </Form>
+        </Container>
+    );
+};
 
 export default Main;
