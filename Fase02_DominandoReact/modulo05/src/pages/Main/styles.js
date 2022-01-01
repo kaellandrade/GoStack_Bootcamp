cf6bbf5c39c29a6fcefb9382e73ab00a4d6e672b
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-01-01 18:32:30 -0300

    estilizando a página man

--- Fase02_DominandoReact/modulo05/src/pages/Main/styles.js
+++ Fase02_DominandoReact/modulo05/src/pages/Main/styles.js
@@ -1,24 +1,5 @@
 import styled, { keyframes, css } from 'styled-components';
 
-const Container = styled.div`
-    max-width: 700px;
-    background-color: #22272e;
-    border-radius: 6px;
-    box-shadow: 0 0 20px rgb(209, 209, 209, 0.1);
-    padding: 30px;
-    margin: 80px auto;
-    border: 1px solid #404852;
-    h1 {
-        color: #d1d1d1;
-        font-size: 25px;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        svg {
-            margin-right: 10px;
-        }
-    }
-`;
 const Form = styled.form`
     margin-top: 30px;
     display: flex;
@@ -74,6 +55,7 @@ const SubmitButton = styled.button.attrs((props) => ({
             }
         `}
 `;
+
 const List = styled.ul`
     list-style: none;
     margin-top: 30px;
@@ -90,8 +72,13 @@ const List = styled.ul`
         a {
             color: #539bf5;
             text-decoration: none;
+            transition: 1s;
+        }
+
+        a:hover {
+            color: #768390;
         }
     }
 `;
 
-export { Container, Form, SubmitButton, List };
+export { Form, SubmitButton, List };
