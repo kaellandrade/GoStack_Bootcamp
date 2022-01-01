da5486e01e1f2e5397b623317958d16ba3889ac8
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-01-01 18:32:45 -0300

    mostrando detalhes do repo

--- Fase02_DominandoReact/modulo05/src/pages/Repository/index.js
+++ Fase02_DominandoReact/modulo05/src/pages/Repository/index.js
@@ -1,9 +1,57 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { FaSpinner, FaArrowLeft } from 'react-icons/fa';
+import { useParams, Link } from 'react-router-dom';
+import API from '../../services/api';
+
+import { Loading, Owner, ButtonBack } from './styles';
+
+import Container from '../../Components/Container/index';
 
 function Repository() {
+    const [StateRepsitory, setRepository] = useState({});
+    const [StateIssues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     const { repository } = useParams();
-    return <h1>Repository:{decodeURIComponent(repository)}</h1>;
+    useEffect(async () => {
+        const repoName = decodeURIComponent(repository);
+
+        // Solicitação juntas, porem em paralelo
+        const [infoRepo, issues] = await Promise.all([
+            API.get(`/repos/${repoName}`),
+            API.get(`/repos/${repoName}/issues`),
+        ]);
+        setRepository(infoRepo.data);
+        setIssues(issues.data);
+        setLoading(false);
+    }, []);
+    if (loading) {
+        return (
+            <Loading>
+                <h1>Carregando</h1>
+                <FaSpinner size={60} />
+            </Loading>
+        );
+    }
+    return (
+        <Container>
+            <ButtonBack>
+                <Link to="/">
+                    <FaArrowLeft size={30} />
+                </Link>
+            </ButtonBack>
+            <Owner>
+                <img
+                    src={StateRepsitory.owner.avatar_url}
+                    alt={StateRepsitory.owner.login}
+                />
+                <div>
+                    <h1>{StateRepsitory.name}</h1>
+                    <p>{StateRepsitory.description}</p>
+                </div>
+            </Owner>
+        </Container>
+    );
 }
 
 export default Repository;
