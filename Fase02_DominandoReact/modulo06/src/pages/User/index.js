c8da38270e7ffc1949c71ab5ff38149e545364a0
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-01-22 11:55:42 -0300

    passsando dados pelas rotas

--- Fase02_DominandoReact/modulo06/src/pages/User/index.js
+++ Fase02_DominandoReact/modulo06/src/pages/User/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const User = () => {
+const User = ({route}) => {
+    console.tron.log(route.params.user)
     return <View />;
 };
 
