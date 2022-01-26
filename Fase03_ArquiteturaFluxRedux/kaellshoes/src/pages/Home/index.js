2f72631e42563998c33de61792a914de98387714
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-01-26 15:08:19 -0300

    estilizando a home

--- Fase03_ArquiteturaFluxRedux/kaellshoes/src/pages/Home/index.js
+++ Fase03_ArquiteturaFluxRedux/kaellshoes/src/pages/Home/index.js
@@ -1,5 +1,89 @@
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
+
 function Home() {
-    return <h1>Home</h1>;
+    return (
+        <ProductList>
+            <li>
+                <figure>
+                    <img src="https://imgcentauro-a.akamaihd.net/250x250/96159631A1/tenis-nike-wearallday-feminino-img.jpg" />
+                    <figcaption>Tênis muito legal.</figcaption>
+                    <span>R$129,90</span>
+                </figure>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <figure>
+                    <img src="https://imgcentauro-a.akamaihd.net/250x250/96159631A1/tenis-nike-wearallday-feminino-img.jpg" />
+                    <figcaption>Tênis muito legal.</figcaption>
+                    <span>R$129,90</span>
+                </figure>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <figure>
+                    <img src="https://imgcentauro-a.akamaihd.net/250x250/96159631A1/tenis-nike-wearallday-feminino-img.jpg" />
+                    <figcaption>Tênis muito legal.</figcaption>
+                    <span>R$129,90</span>
+                </figure>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <figure>
+                    <img src="https://imgcentauro-a.akamaihd.net/250x250/96159631A1/tenis-nike-wearallday-feminino-img.jpg" />
+                    <figcaption>Tênis muito legal.</figcaption>
+                    <span>R$129,90</span>
+                </figure>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <figure>
+                    <img src="https://imgcentauro-a.akamaihd.net/250x250/96159631A1/tenis-nike-wearallday-feminino-img.jpg" />
+                    <figcaption>Tênis muito legal.</figcaption>
+                    <span>R$129,90</span>
+                </figure>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <figure>
+                    <img src="https://imgcentauro-a.akamaihd.net/250x250/96159631A1/tenis-nike-wearallday-feminino-img.jpg" />
+                    <figcaption>Tênis muito legal.</figcaption>
+                    <span>R$129,90</span>
+                </figure>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+        </ProductList>
+    );
 }
 
 export default Home;
