f3daea0a4930b1a1d9b5c3222beeaa119ab97104
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-01-26 11:21:59 -0300

    estilos globais

--- Fase03_ArquiteturaFluxRedux/kaellshoes/src/index.js
+++ Fase03_ArquiteturaFluxRedux/kaellshoes/src/index.js
@@ -1,5 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from './App';
+import GlobalStyles from './styles/global';
+
+ReactDOM.render(
+    <Fragment>
+        <App />
+        <GlobalStyles />
+    </Fragment>,
+    document.getElementById('root')
+);
