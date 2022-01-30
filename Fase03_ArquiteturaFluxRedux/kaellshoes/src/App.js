9c8e3d7dd0e488655ecc361dde254c43c1fe018d
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-01-30 08:51:33 -0300

    conectando com reactotron

--- Fase03_ArquiteturaFluxRedux/kaellshoes/src/App.js
+++ Fase03_ArquiteturaFluxRedux/kaellshoes/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Routes from './routes';
 import Header from './components/Header';
-
+import './config/ReactotronConfig';
 import store from './store';
 
 const App = () => (
