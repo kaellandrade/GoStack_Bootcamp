77eba4b1b85c61b9c3ecb762bf55f6d0e78c25ca
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-03-05 00:46:22 -0300

    criando as paginas

---
+++ Fase03_ArquiteturaFluxRedux/desafio07/kaellShoesMobile/src/pages/Cart.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export default (_) => {
+    return (
+        <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        >
+            <Text style={{ color: 'red' }}>Carrinho!</Text>
+        </View>
+    );
+};
