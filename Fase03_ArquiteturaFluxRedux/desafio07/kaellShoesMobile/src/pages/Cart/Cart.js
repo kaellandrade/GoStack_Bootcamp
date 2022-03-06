311b8eee5c27b8fdd6f5d2a542e4df9656159509
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-03-05 22:37:44 -0300

    Criando copmentes carrinho

---
+++ Fase03_ArquiteturaFluxRedux/desafio07/kaellShoesMobile/src/pages/Cart/Cart.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Container } from './styles';
+
+import EmptyCart from '../../components/Cart/EmptyCart';
+
+export default (_) => (
+    <Container>
+        <EmptyCart />
+    </Container>
+);
