2f4a5081074c5ebfa62e958fde3aa2fbbfeda094
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-04-01 23:44:21 -0300

    vinculando o app com redux

--- Fase03_ArquiteturaFluxRedux/desafio07/kaellShoesMobile/App.js
+++ Fase03_ArquiteturaFluxRedux/desafio07/kaellShoesMobile/App.js
@@ -1,16 +1,21 @@
 import './src/config/ReactotronConfig';
+import {Provider} from 'react-redux'
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './src/routes';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import store from './src/store';
 
 function App(_) {
     return (
+        <Provider store={store}>
+
         <GestureHandlerRootView resizeMode="center" style={{ flex: 1 }}>
             <NavigationContainer>
                 <AppRoutes />
             </NavigationContainer>
         </GestureHandlerRootView>
+        </Provider>
     );
 }
 
