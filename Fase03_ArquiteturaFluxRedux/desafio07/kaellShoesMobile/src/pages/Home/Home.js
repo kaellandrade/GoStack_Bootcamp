2d8e2cefdc8779fba7711031793cce74ad55dfd6
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-04-01 23:44:10 -0300

    recuperando dados da API

--- Fase03_ArquiteturaFluxRedux/desafio07/kaellShoesMobile/src/pages/Home/Home.js
+++ Fase03_ArquiteturaFluxRedux/desafio07/kaellShoesMobile/src/pages/Home/Home.js
@@ -1,21 +1,45 @@
-import * as React from 'react';
+import React from 'react';
+import { Alert } from 'react-native';
+import { useState, useEffect } from 'react';
 import { Container, ProductsList } from './styles';
 import Card from '../../components/Card/card';
-import DATA from '../../../server.json';
 import getNumbersRows from '../../util/getNumbersRows';
 import { CARD_SIZE, MARGIN_CARD } from '../../util/consts';
+import API from '../../services/api';
+import { formatPrice } from '../../util/format';
+
+/**
+ * Recupera meus produtos da API
+ */
+const getProducts = async (setProducts) => {
+    try {
+        const response = await API.get('products');
+        const data = response.data.map((product) => ({
+            ...product,
+            priceFormatted: formatPrice(product.price),
+        }));
+        setProducts(data);
+    } catch (error) {
+        Alert.alert('Error', 'Error ao recuperar os produtos', false);
+    }
+};
 
 export default (_) => {
+    const [products, setProducts] = useState([]);
+    useEffect((_) => {
+        getProducts(setProducts);
+    }, []);
+
     return (
         <Container>
             <ProductsList
-                data={DATA.products}
+                data={products}
                 numColumns={getNumbersRows(CARD_SIZE + MARGIN_CARD)}
                 contentContainerStyle={{ alignItems: 'center' }}
                 renderItem={({ item }) => (
                     <Card
                         title={item.title}
-                        price={item.price}
+                        price={item.priceFormatted}
                         srcImg={item.image}
                     />
                 )}
