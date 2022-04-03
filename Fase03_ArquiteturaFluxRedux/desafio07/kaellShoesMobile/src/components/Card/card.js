e3d97eba2a2320b8ae537c94766fbe3546aadaff
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-04-03 17:39:27 -0300

    funcoes de adicionar ao carrrinho

--- Fase03_ArquiteturaFluxRedux/desafio07/kaellShoesMobile/src/components/Card/card.js
+++ Fase03_ArquiteturaFluxRedux/desafio07/kaellShoesMobile/src/components/Card/card.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import propsTypes from 'prop-types';
-import {BtnICon, BtnText} from '../Btns/Btns';
+import { BtnICon, BtnText } from '../Btns/Btns';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Text } from 'react-native';
 
-import { Container, ImgProduct, TitleProduct, Price,GroupButton  } from './styles';
+import {
+    Container,
+    ImgProduct,
+    TitleProduct,
+    Price,
+    GroupButton,
+    TextDescription,
+} from './styles';
 
-const Card = ({ srcImg, title, price }) => {
+const Card = ({ srcImg, title, price, totByItem,id, handleAddProduct }) => {
     return (
         <Container>
             <ImgProduct source={{ uri: srcImg }} />
@@ -15,10 +22,10 @@ const Card = ({ srcImg, title, price }) => {
             <GroupButton>
                 <BtnICon>
                     <Icon name="cart" color={'#ececec'} size={30} />
-                    <Text>2</Text>
+                    <TextDescription>{totByItem[id] || 0}</TextDescription>
                 </BtnICon>
-                <BtnText>
-                    <Text>Adicionar ao Carrinho</Text>
+                <BtnText handleAddProduct={(_) => handleAddProduct(id)}>
+                    <TextDescription>Adicionar ao carrinho</TextDescription>
                 </BtnText>
             </GroupButton>
         </Container>
