70535b09c8eaf3b7a072b11890adf4de627f7bc6
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-04-03 17:38:39 -0300

    contabilizando o carrinho

--- Fase03_ArquiteturaFluxRedux/desafio07/kaellShoesMobile/src/routes.js
+++ Fase03_ArquiteturaFluxRedux/desafio07/kaellShoesMobile/src/routes.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useSelector } from 'react-redux';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Cart from './pages/Cart/Cart';
@@ -6,33 +7,43 @@ import Home from './pages/Home/Home';
 
 const Tab = createBottomTabNavigator();
 
-const screenOptions = ({ route }) => ({
-    tabBarIcon: ({ focused, color, size }) => {
-        let iconName;
+const screenOptions = ({ route }) => {
+    return {
+        tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-        if (route.name === 'Loja') {
-            iconName = focused ? 'card' : 'card-outline';
-        } else if (route.name === 'Carrinho') {
-            iconName = focused ? 'cart' : 'cart-outline';
-        }
+            if (route.name === 'Loja') {
+                iconName = focused ? 'card' : 'card-outline';
+            } else if (route.name === 'Carrinho') {
+                iconName = focused ? 'cart' : 'cart-outline';
+            }
 
-        return <Ionicons name={iconName} size={size} color={color} />;
-    },
-    tabBarInactiveTintColor: '#9b9a9f',
-    tabBarActiveTintColor: '#17161b',
-    tabBarStyle: { backgroundColor: '#ffffff', position: 'relative' },
-    tabBarBadgeStyle: { backgroundColor: '#634db6' },
-    headerShown: false
-});
+            return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarInactiveTintColor: '#9b9a9f',
+        tabBarActiveTintColor: '#17161b',
+        tabBarStyle: { backgroundColor: '#ffffff', position: 'relative' },
+        tabBarBadgeStyle: { backgroundColor: '#634db6' },
+        headerShown: false,
+    };
+};
 
-const AppRoutes = (_) => (
-    <Tab.Navigator screenOptions={screenOptions}>
-        <Tab.Screen name="Loja" component={Home} />
-        <Tab.Screen
-            name="Carrinho"
-            component={Cart}
-            options={{ tabBarBadge: 1 }}
-        />
-    </Tab.Navigator>
-);
+const AppRoutes = (_) => {
+    const totalAmount = useSelector(({ cart }) => cart).reduce(
+        (ccur, atual) => ccur + atual.amount,
+        0
+    );
+    console.tron.log(totalAmount)
+
+    return (
+        <Tab.Navigator screenOptions={screenOptions}>
+            <Tab.Screen name="Loja" component={Home} />
+            <Tab.Screen
+                name="Carrinho"
+                component={Cart}
+                options={{ tabBarBadge: totalAmount }}
+            />
+        </Tab.Navigator>
+    );
+};
 export { AppRoutes };
