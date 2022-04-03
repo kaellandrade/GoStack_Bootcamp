d3a0b2c89d8793223e477733353fb010f417a849
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-04-03 17:38:59 -0300

    ajustando components estilos

--- Fase03_ArquiteturaFluxRedux/desafio07/kaellShoesMobile/src/components/Btns/styles.js
+++ Fase03_ArquiteturaFluxRedux/desafio07/kaellShoesMobile/src/components/Btns/styles.js
@@ -2,15 +2,15 @@ import styled from 'styled-components';
 import { RectButton } from 'react-native-gesture-handler';
 
 export const BtnRect = styled(RectButton)`
-    flex: .2;
     flex-direction: row;
     align-items: center;
     opacity: ${(props) => (props.loading ? 0.7 : 1)};
     background-color: #5b42b4;
+    padding: 0 10px 0;
 `;
 export const BtnRectText = styled(RectButton)`
     opacity: ${(props) => (props.loading ? 0.7 : 1)};
     background-color: #624db6;
     justify-content: center;
-    flex: .8;
+    flex: 1;
 `;
