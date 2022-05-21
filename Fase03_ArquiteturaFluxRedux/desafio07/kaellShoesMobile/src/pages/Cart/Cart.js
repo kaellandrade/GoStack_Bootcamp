8ee25c901259e0f745f81d2bfdc5cd9ee212e2b1
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-05-21 00:33:28 -0300

    Renderizando carrinho.

--- Fase03_ArquiteturaFluxRedux/desafio07/kaellShoesMobile/src/pages/Cart/Cart.js
+++ Fase03_ArquiteturaFluxRedux/desafio07/kaellShoesMobile/src/pages/Cart/Cart.js
@@ -1,10 +1,23 @@
 import * as React from 'react';
-import { Container } from './styles';
+import {useSelector} from "react-redux";
+import {Container} from './styles';
 
 import EmptyCart from '../../components/Cart/EmptyCart';
+import {Text} from "react-native";
 
-export default (_) => (
-    <Container>
-        <EmptyCart />
-    </Container>
-);
+export default (_) => {
+
+
+    const cartDatas = useSelector(({cart}) => cart);
+    console.log(cartDatas);
+    const total = cartDatas.reduce((acumulador, {amount})=> acumulador + amount, 0);
+    return cartDatas.length
+        ?
+        <Container>
+            <Text>{total}</Text>
+        </Container>
+        :
+        <Container>
+            <EmptyCart/>
+        </Container>
+};
