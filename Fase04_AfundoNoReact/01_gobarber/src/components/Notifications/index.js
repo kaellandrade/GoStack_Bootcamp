cae2a4576849a5e241b012ef306233f6226a5b67
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2022-07-27 23:07:37 -0300

    Animando sino das notificações e marcando como lida

--- Fase04_AfundoNoReact/01_gobarber/src/components/Notifications/index.js
+++ Fase04_AfundoNoReact/01_gobarber/src/components/Notifications/index.js
@@ -1,30 +1,49 @@
-import {MdNotifications} from 'react-icons/md'
 import {useEffect, useState} from "react";
 import {parseISO, formatDistance} from 'date-fns';
 import pt from 'date-fns/locale/pt';
-import {Container, Badge, Scroll, Notification, NotificationsList} from './styles';
+import {Container, Badge, NumberNotification, Scroll, Notification, NotificationsList, AnimationBell} from './styles';
 import api from "../../services/api";
 
-const loadNotifications = async setNotification => {
-	const response = await api.get('notifications');
+const countUnread = notifications => notifications.reduce((acc, {read}) => !read ? acc + 1 : acc, 0);
 
-	const data = response.data.map(notification => ({
-		...notification,
-		timeDistance: formatDistance(
-			parseISO(notification.createdAt),
-			new Date(),
-			{addSuffix:true, locale:pt}
-		)
-	}))
-	setNotification(data);
-}
+
 const Index = () => {
 	const [visible, setVisable] = useState(true);
-	const [notifications, setNotification] = useState(true);
+	const [notifications, setNotification] = useState([]);
+	const [totalNotificationUnread, setTotal] = useState(0);
 
-	useEffect(()=>{
-		loadNotifications(setNotification);
-	},[]);
+	const loadNotifications = async _ => {
+		const response = await api.get('notifications');
+
+		const data = response.data.map(notification => ({
+			...notification,
+			timeDistance: formatDistance(
+				parseISO(notification.createdAt),
+				new Date(),
+				{addSuffix: true, locale: pt}
+			)
+		}))
+		setNotification(data);
+		setTotal(countUnread(data));
+
+	}
+
+	const handleMarkAsRead = async (idNotification) => {
+		await api.put(`notifications/${idNotification}`);
+		const newNotifications = notifications.map((notification) => {
+			// eslint-disable-next-line no-underscore-dangle
+			if (notification._id === idNotification) {
+				return {...notification, read: true}
+			}
+			return notification
+		})
+		setNotification(newNotifications);
+		setTotal(countUnread(newNotifications));
+	}
+
+	useEffect(() => {
+		loadNotifications();
+	}, []);
 
 
 	const toggleVisable = _ => {
@@ -33,40 +52,29 @@ const Index = () => {
 
 	return (
 		<Container>
-			<Badge onClick={toggleVisable} hasUnRead={true}>
-				<MdNotifications color="#7159c1" size={20}/>
+			<Badge onClick={toggleVisable}>
+				{totalNotificationUnread ?
+					<NumberNotification>
+						{totalNotificationUnread}
+					</NumberNotification>
+					: null
+				}
+				<AnimationBell sing={totalNotificationUnread} color="#7159c1" size={25}/>
 			</Badge>
-			<NotificationsList visible ={visible}>
+			<NotificationsList visible={visible}>
 				<Scroll>
-					<Notification unread>
-						<p>Você possui um novo agendamento para hoje.</p>
-						<time>há 2 dias</time>
-						<button type="button">Marcar como lida</button>
-					</Notification>
-
-					<Notification>
-						<p>Você possui um novo agendamento para hoje.</p>
-						<time>há 2 dias</time>
-						<button type="button">Marcar como lida</button>
-					</Notification>
-
-					<Notification>
-						<p>Você possui um novo agendamento para hoje.</p>
-						<time>há 2 dias</time>
-						<button type="button">Marcar como lida</button>
-					</Notification>
-
-					<Notification>
-						<p>Você possui um novo agendamento para hoje.</p>
-						<time>há 2 dias</time>
-						<button type="button">Marcar como lida</button>
-					</Notification>
-
-					<Notification>
-						<p>Você possui um novo agendamento para hoje.</p>
-						<time>há 2 dias</time>
-						<button type="button">Marcar como lida</button>
-					</Notification>
+					{notifications.map(({content, read, timeDistance, _id}) => (
+							<Notification key={_id} unread={!read}>
+								<p>{content}</p>
+								<time>{timeDistance}</time>
+								{!read &&
+									<button onClick={_ => handleMarkAsRead(_id)}
+											type="button">Marcar como lida
+									</button>
+								}
+							</Notification>
+						)
+					)}
 				</Scroll>
 			</NotificationsList>
 		</Container>
--- Fase04_AfundoNoReact/01_gobarber/src/components/Notifications/styles.js
+++ Fase04_AfundoNoReact/01_gobarber/src/components/Notifications/styles.js
@@ -1,6 +1,106 @@
-import styled, {css} from 'styled-components';
+import styled, {css, keyframes} from 'styled-components';
 import {lighten} from 'polished'
 import PerfectScrollBar from 'react-perfect-scrollbar';
+import {MdNotifications} from 'react-icons/md'
+
+
+const bellAnimation = keyframes`
+  0% {
+    transform: rotateZ(0);
+  }
+  1% {
+    transform: rotateZ(30deg);
+  }
+  3% {
+    transform: rotateZ(-28deg);
+  }
+  5% {
+    transform: rotateZ(34deg);
+  }
+  7% {
+    transform: rotateZ(-32deg);
+  }
+  9% {
+    transform: rotateZ(30deg);
+  }
+  11% {
+    transform: rotateZ(-28deg);
+  }
+  13% {
+    transform: rotateZ(26deg);
+  }
+  15% {
+    transform: rotateZ(-24deg);
+  }
+  17% {
+    transform: rotateZ(22deg);
+  }
+  19% {
+    transform: rotateZ(-20deg);
+  }
+  21% {
+    transform: rotateZ(18deg);
+  }
+  23% {
+    transform: rotateZ(-16deg);
+  }
+  25% {
+    transform: rotateZ(14deg);
+  }
+  27% {
+    transform: rotateZ(-12deg);
+  }
+  29% {
+    transform: rotateZ(10deg);
+  }
+  31% {
+    transform: rotateZ(-8deg);
+  }
+  33% {
+    transform: rotateZ(6deg);
+  }
+  35% {
+    transform: rotateZ(-4deg);
+  }
+  37% {
+    transform: rotateZ(2deg);
+  }
+  39% {
+    transform: rotateZ(-1deg);
+  }
+  41% {
+    transform: rotateZ(1deg);
+  }
+
+  43% {
+    transform: rotateZ(0);
+  }
+  100% {
+    transform: rotateZ(0);
+  }`;
+
+export const AnimationBell = styled(MdNotifications)`
+
+  ${props => props.sing && css`
+    animation-name: ${bellAnimation};
+    animation-duration: 3s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+  `}`;
+export const NumberNotification = styled.span`
+  border-radius: 30px;
+  padding: 0px 3px 0;
+  position: absolute;
+  right: -12px;
+  top: -5px;
+  //width: 8px;
+  //height: 8px;
+  background: #ff892e;
+  z-index: 1;
+  color: #ffffff;
+  box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.25);
+  font-weight: bold;
+`;
 
 export const Scroll = styled(PerfectScrollBar)`
   max-height: 260px;
@@ -16,20 +116,22 @@ export const NotificationsList = styled.div`
   background: rgb(0, 0, 0, 0.6);
   border-radius: 4px;
   padding: 15px 5px;
-  display: ${({visible}) => visible ? 'block': 'none'};
-  };
+  display: ${({visible}) => visible ? 'block' : 'none'};
+}
 
-  &::before {
-    content: '';
-    position: absolute;
-    left: calc(50% - 20px);
-    top: -20px;
-    width: 0;
-    height: 0;
-    border-left: 20px solid transparent;
-    border-right: 20px solid transparent;
-    border-bottom: 20px solid rgb(0, 0, 0, 0.6);
-  }
+;
+
+&::before {
+  content: '';
+  position: absolute;
+  left: calc(50% - 20px);
+  top: -20px;
+  width: 0;
+  height: 0;
+  border-left: 20px solid transparent;
+  border-right: 20px solid transparent;
+  border-bottom: 20px solid rgb(0, 0, 0, 0.6);
+}
 `;
 export const Notification = styled.div`
   color: #fff;
@@ -48,6 +150,7 @@ export const Notification = styled.div`
   time {
     font-size: 12px;
     opacity: 0.6;
+    display: block;
   }
 
   button {
@@ -55,20 +158,18 @@ export const Notification = styled.div`
     border: 0;
     background: none;
     color: ${lighten(0.2, '#7159c1')};
-    padding: 0 5px;
-    margin: 0 5px;
-    border-left: 1px solid rgb(255, 255, 255, 0.1);
   }
 
   ${props => props.unread && css`
     &::after {
       content: '';
-      display: block;
+      display: inline-block;
       margin-left: 5px;
       width: 8px;
       height: 8px;
       background: #ff892e;
       border-radius: 50%;
+      margin-left: 10px;
     }
   `}
 `;
@@ -81,17 +182,4 @@ export const Badge = styled.button`
   background: none;
   border: 0;
   position: relative;
-
-  ${props => props.hasUnRead && css`
-    &::after {
-      position: absolute;
-      right: 0;
-      top: 0;
-      width: 8px;
-      height: 8px;
-      background: #ff892e;
-      content: '';
-      border-radius: 50%;
-    }
-  `}
 `;
