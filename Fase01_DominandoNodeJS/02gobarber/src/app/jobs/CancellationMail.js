7ddf7b6fe093db21f88d033d408a2c463e68fb33
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2022-08-20 03:01:58 -0300

    Configurando email de cancelamento

--- Fase01_DominandoNodeJS/02gobarber/src/app/jobs/CancellationMail.js
+++ Fase01_DominandoNodeJS/02gobarber/src/app/jobs/CancellationMail.js
@@ -1,33 +1,37 @@
-import { format, parseISO } from 'date-fns';
+import {format, parseISO} from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import Mail from '../../lib/Mail';
 
 class CancellationMail {
-    get key() {
-        return 'CancellationMail';
-    }
-    /**
-     * Tarefa que será executada
-     */
-    async handle({ data }) {
-        const { appointment } = data;
-        await Mail.sendMail({
-            to: `${appointment.provider.name} <${appointment.provider.email}>`,
-            subject: 'Agendamento Cancelado',
-            template: 'cancellation',
-            context: {
-                provider: appointment.provider.name,
-                user: appointment.user.name,
-                date: format(
-                    parseISO(appointment.date),
-                    "dd 'de' MMMM', às' H:mm'h' ",
-                    {
-                        locale: pt,
-                    },
-                ),
-            },
-        });
-    }
+  get key() {
+    return 'CancellationMail';
+  }
+
+  /**
+   * Tarefa que será executada
+   */
+  async handle({data}) {
+    const {appointment} = data;
+    const linkAvatar = `http://localhost:3333/files/${appointment.user.avatar.path}`;
+    console.log(appointment);
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento Cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        avatar: linkAvatar,
+        date: format(
+          parseISO(appointment.date),
+          "dd 'de' MMMM', às' H:mm'h' ",
+          {
+            locale: pt,
+          },
+        ),
+      },
+    });
+  }
 }
 
 export default new CancellationMail();
